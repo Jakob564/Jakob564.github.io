5db3a97b7419f933516b4a677a0f1ca6ab23d6ad
Jakob Klaffenböck <Jakob564>
2022-11-22 12:28:29 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+function showRecipe() {
+    erg.innerHTML =         
+    ' <h2>Step 1: Lorem ipsum dolor sit amet.</h2> '
+    + '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus voluptas aliquam deleniti nisi quasi accusantium, quaerat ipsa dolorum impedit ab praesentium, voluptatum, itaque optio officiis quam. Molestias reprehenderit quod earum. '
+    + 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla molestiae est eos aliquid, consequuntur ea iusto vero consequatur nostrum quibusdam modi quidem pariatur, laborum tenetur iste quae excepturi ab impedit. '
+    + 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus eveniet quasi neque quae ipsam inventore! Incidunt blanditiis dolorum, quibusdam fugiat unde dolor odio ab consequatur! Quae asperiores tenetur unde ipsam? '
+    + 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque ex nemo eius animi rem fuga soluta est, numquam ab quasi harum ullam voluptatibus voluptatem nihil consequuntur quos repellat provident! Optio! '
+    + '</p><br>'
+
+    + '<h2>Step 2: Lorem ipsum dolor sit amet.</h2>'
+    + '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus voluptas aliquam deleniti nisi quasi accusantium, quaerat ipsa dolorum impedit ab praesentium, voluptatum, itaque optio officiis quam. Molestias reprehenderit quod earum. '
+    + 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla molestiae est eos aliquid, consequuntur ea iusto vero consequatur nostrum quibusdam modi quidem pariatur, laborum tenetur iste quae excepturi ab impedit. '
+    + 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus eveniet quasi neque quae ipsam inventore! Incidunt blanditiis dolorum, quibusdam fugiat unde dolor odio ab consequatur! Quae asperiores tenetur unde ipsam? '
+    + 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque ex nemo eius animi rem fuga soluta est, numquam ab quasi harum ullam voluptatibus voluptatem nihil consequuntur quos repellat provident! Optio! '
+    + '</p><br>'
+
+    + '<h2>Step 3: Lorem ipsum dolor sit amet.</h2>'
+    + '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus voluptas aliquam deleniti nisi quasi accusantium, quaerat ipsa dolorum impedit ab praesentium, voluptatum, itaque optio officiis quam. Molestias reprehenderit quod earum. '
+    + 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla molestiae est eos aliquid, consequuntur ea iusto vero consequatur nostrum quibusdam modi quidem pariatur, laborum tenetur iste quae excepturi ab impedit. '
+    + 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus eveniet quasi neque quae ipsam inventore! Incidunt blanditiis dolorum, quibusdam fugiat unde dolor odio ab consequatur! Quae asperiores tenetur unde ipsam? '
+    + 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque ex nemo eius animi rem fuga soluta est, numquam ab quasi harum ullam voluptatibus voluptatem nihil consequuntur quos repellat provident! Optio! '
+    + '</p><br>'
+}
