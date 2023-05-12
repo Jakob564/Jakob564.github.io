664dbe0cb5028bb310b82ae7cc5748fa8f48f0b4
unknown <j.klaffenboeck@students.htl-leonding.ac.at>
2023-05-12 08:06:55 +0200

    Initial commit

--- script/script.js
+++ script/script.js
@@ -46,11 +46,10 @@ window.initMap = initMap;
 
 //Autocomplete
 
-let autocomplete;
 initAutocomplete();
 
 function initAutocomplete() {
-  autocomplete = new google.maps.places.Autocomplete(
+  let autocomplete = new google.maps.places.Autocomplete(
     document.getElementById("destination"),
     {
       types: ['establishment'],
